7c7bd401f0acd97c8c7ee1f9edb5d9f18d254f41
gk-svg <gk32239@gmail.com>
2020-05-01 19:48:24 +0530

    desc converted into textfield

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
     writter = models.CharField(max_length=50,default="")
     title = models.CharField(max_length=100)
-    desc = models.CharField(max_length=500,default="")
+    desc = models.TextField(max_length=500,default="")
     contant = models.TextField(max_length=5000)
     pub_date = models.DateTimeField(auto_now_add=True)
     thumbnail = models.ImageField(upload_to='blog/images', default="")
