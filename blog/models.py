31ff7c951a4dc472a9432d6de7177380c27daffc
gk-svg <gk32239@gmail.com>
2020-05-02 09:39:32 +0530

    slug added and desc removed

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,9 @@ class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
     writter = models.CharField(max_length=50,default="")
     title = models.CharField(max_length=100)
-    desc = models.TextField(max_length=500,default="")
     contant = models.TextField(max_length=5000)
     pub_date = models.DateTimeField(auto_now_add=True)
+    slug = models.CharField(max_length=130,default="")
     thumbnail = models.ImageField(upload_to='blog/images', default="")
 
     def __str__(self):
