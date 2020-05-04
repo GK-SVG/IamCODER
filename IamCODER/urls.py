1a08f49a03204222f41dbaf1fdcf33d4f10d2252
gk-svg <gk32239@gmail.com>
2020-05-04 12:24:11 +0530

    admin urls fixed

--- IamCODER/urls.py
+++ IamCODER/urls.py
@@ -1,4 +1,4 @@
-"""IamCODER URL Configuration
+"""accounts URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -17,8 +17,9 @@ from django.urls import path,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static  
+
 urlpatterns = [
-     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',include('blog.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
