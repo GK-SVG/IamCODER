f23da59ae80a32e05034eaf39c78c829287b4ebd
gk-svg <gk32239@gmail.com>
2020-05-05 16:48:16 +0530

    Blogcomment url created

--- blog/urls.py
+++ blog/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"), 
     path("login/", views.handlelogin, name="handlelogin"),
     path("logout/", views.handlelogout, name="handlelogout"),
+    path("postComment/", views.postComment, name="postComment"),
 ]
 
