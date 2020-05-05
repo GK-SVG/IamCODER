bd8d7647ef731f11a80ec4880d4d43b78541713e
gk-svg <gk32239@gmail.com>
2020-05-05 12:22:37 +0530

    Comments Model registered to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Blogpost
+from .models import Blogpost,BlogCommet
 admin.site.register(Blogpost)
+admin.site.register(BlogCommet)
