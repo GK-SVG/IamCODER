f41dbec5b40b535eb1b2392f8c4cffb29e4f99f1
gk-svg <gk32239@gmail.com>
2020-05-07 18:59:01 +0530

    tinyMCN editor injected to admin panal

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Blogpost,BlogCommet
-admin.site.register(Blogpost)
 admin.site.register(BlogCommet)
+
+@admin.register(Blogpost)
+class BlogpostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('tinyInject.js',)
