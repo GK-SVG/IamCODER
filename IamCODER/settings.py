5040b6e47d0fb78d9d1ae95d34a23ea2a29ce7e1
gk-svg <gk32239@gmail.com>
2021-05-15 20:17:34 +0530

    heroku deployment debug false

--- IamCODER/settings.py
+++ IamCODER/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'IamCODER.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
